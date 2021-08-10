ec5526f56c8248e81496b90a9224b34f8948336e
ZhiangChen <zxc251@case.edu>
2021-08-09 22:30:28 -0700

    edited simple waypoints

--- src/waypoint_generator.py
+++ src/waypoint_generator.py
@@ -31,8 +31,15 @@ if __name__ == '__main__':
     rospy.init_node('waypoints_client', anonymous=False)
     rospy.wait_for_service('stop_sampling')
     stop_srv_client_ = rospy.ServiceProxy('stop_sampling', Empty)
-    positions = np.asarray(((0, 0, 5), (4, 4, 10), (0, 8, 10), (-4, 4, 10), (0, 0, 10), (4, -4, 5), (0, -8, 5), (-4, -4, 5), (0, 0, 5)))
-    yaws = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    positions = np.asarray(((0, 0, 10), (4, 4, 10), (0, 8, 10), (-4, 4, 10), (0, 0, 10), (4, -4, 5), (0, -8, 5), (-4, -4, 5), (0, 0, 5)))
+    yaws = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    #positions = np.asarray(((0, 0, 1.5), (-10, -10, 2), (0, 0, 1)))
+    #yaws = [-0.5, -0., -0.5]
+
+    #positions = np.asarray(( (-13.5, 0, 10), (-15, 0, 10),))
+    #yaws = [-3, -3]
+    #positions = np.asarray(((-18, 0, 11), (-18, 1, 11), (-10, 1, 11)))
+    #yaws = [-3, 0, 0]
     result = waypoints_client(positions, yaws)
     #rospy.sleep(5.)
     #positions = np.asarray(((-4, 4, 10), (0, 0, 10), (4, -4, 5), (0, -8, 5), (-4, -4, 5), (0, 0, 5)))
